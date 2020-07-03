6b757af78d725bae91ae6629f3e8db4c35c817df
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-07-03 11:30:47 +0530

    Arranging coins

---
+++ arranging_coins.js
@@ -0,0 +1,15 @@
+const arrangeCoins = function (n) {
+    let i = 1;
+    let j = 0;
+    while (n > 0) {
+        n = n - i
+        i++
+        k++
+    }
+
+    if (n === 0) {
+        return k
+    } else {
+        return --k
+    }
+}
