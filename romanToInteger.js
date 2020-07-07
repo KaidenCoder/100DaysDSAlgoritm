d6c3bca7adc03b7ddcb37ca2e5f5dbad368645ac
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-07-07 21:16:56 +0530

    secondromanint

--- romanToInteger.js
+++ romanToInteger.js
@@ -1,17 +1,17 @@
-const romanToInt = function (s) {
+const romanToInt = function (s) {   // s = IV
     if (s == null) {
         return -1
     }
-    let num = integerConverter(s.charAt(0))
+    let num = integerConverter(s.charAt(0)) // num = I == 1
     let pre, curr;
 
     for (let i = 1; i < s.length; i++) {
-        curr = integerConverter(s.charAt(i))
-        pre = integerConverter(s.charAt(i - 1))
-        if (curr <= pre) {
+        curr = integerConverter(s.charAt(i)) // curr = V == 5
+        pre = integerConverter(s.charAt(i - 1)) // pre = I == 1
+        if (curr <= pre) { // curr = 5 > pre = 1 -- going to else
             num += curr
         } else {
-            num = num - pre * 2 + curr
+            num = num - pre * 2 + curr // num = 1 - 1*2 + 5 = 6 - 2 = 4
         }
     }
     return num
@@ -30,4 +30,4 @@ function integerConverter(c) {
     }
 }
 
-romanToInt('XI')
+romanToInt('IV')
