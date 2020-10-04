8d9cf2172ce5ca1f336bdb227d3cfecb6ed7965e
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-04 16:37:15 +0530

    reshape matrix notes

--- reshapeMatrix.py
+++ reshapeMatrix.py
@@ -1,3 +1,16 @@
+# Example 1:
+
+# Input:
+# nums =
+# [[1,2],
+#  [3,4]]
+# r = 1, c = 4
+# Output:
+# [[1,2,3,4]]
+# Explanation:
+# The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+
+
 class Solution:
     def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
         num_list = []
