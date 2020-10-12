de887e2529ed92f7809f1acf066c2c8ccd55136d
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-12 10:59:19 +0530

    Spiral

--- SpiralMatrix.py
+++ SpiralMatrix.py
@@ -14,7 +14,7 @@ class Solution:
         res = []
         if len(matrix) == 0:
             return res
-        row_begin, row_end = 0, len(matrix)-1
+        row_begin, row_end = 0, len(matrix)-1  # 0,2
         col_begin, col_end = 0, len(matrix[0])-1
         while (row_begin <= row_end and col_begin <= col_end):
             for i in range(col_begin, col_end+1):
@@ -24,13 +24,13 @@ class Solution:
                 res.append(matrix[i][col_end])
             col_end -= 1
             if (row_begin <= row_end):
-                for i in range(col_end, col_begin-1, -1):
-                    # for i in reversed(range(col_begin,col_end)):
+                # for i in range(col_end,col_begin-1,-1):
+                for i in reversed(range(col_begin, col_end+1)):  # 2,-1
                     res.append(matrix[row_end][i])
                 row_end -= 1
             if (col_begin <= col_end):
-                for i in range(row_end, row_begin-1, -1):
-                    # for i in reversed(range(row_begin+1,row_end)):
+                # for i in range(row_end,row_begin-1,-1):
+                for i in reversed(range(row_begin, row_end+1)):
                     res.append(matrix[i][col_begin])
                 col_begin += 1
         return res
