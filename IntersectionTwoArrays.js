4076b6326a6d9acca85a979452d6bce6752c0b5d
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-26 08:11:10 +0530

    Intersection of two arrays

---
+++ IntersectionTwoArrays.js
@@ -0,0 +1,32 @@
+// Given two arrays, write a function to compute their intersection.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [9,4]
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+    let res = []
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] == nums2[j]) {
+                res.push(nums2[j])
+            }
+        }
+    }
+
+    let resSet = new Set(res)
+    let finalres = [...resSet]
+    return finalres
+};
