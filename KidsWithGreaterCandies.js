784b02f3d2e5d2e81f90e50c8ed3f5b38ea44a0e
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-11-17 21:38:07 +0530

    Kids With the Greatest Number of Candies

---
+++ KidsWithGreaterCandies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+    let res = []
+    let max = Math.max(...candies)
+    for (let i = 0; i < candies.length; i++) {
+        if ((candies[i] + extraCandies) >= max) {
+            res.push(true)
+        } else {
+            res.push(false)
+        }
+    }
+    return res
+};
